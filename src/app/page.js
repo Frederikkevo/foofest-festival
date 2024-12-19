8ad63803f3d52a9fc7f4d87becb879a8774773c1
Frederikke¨ <frvo0001@stud.kea.dk>
2024-12-19 21:13:23 +0100

    forside billede

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-
 import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <main className="p-8">
+    <main className="relative w-full min-h-screen p-8 bg-cover bg-center" style={{ backgroundImage: "url('/images/forside.webp')" }}>
       <Header />
-      <h2 className="text-primary text-3xl font-bold mt-6">Welcome to Foofest!</h2>
-      <p className="mt-4">Join us for an unforgettable experience of music, art, and community.</p>
+
     </main>
   );
 }
