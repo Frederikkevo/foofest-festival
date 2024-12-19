f60d2151af1e365e4c921d5e2d2114cead336fc9
Frederikke¨ <frvo0001@stud.kea.dk>
2024-12-19 20:51:40 +0100

    link

--- src/app/tickets/page.js
+++ src/app/tickets/page.js
@@ -4,7 +4,7 @@ export default function Tickets() {
     <main className="p-8">
       <h1 className="text-accent text-3xl font-bold mb-4">Tickets</h1>
       <p>Get your tickets now and join the fun!</p>
-      <a href="https://hill-mirror-era.glitch.me/tickets" className="text-primary underline mt-4 block">
+      <a href="https://LINK HER!" className="text-primary underline mt-4 block">
         Buy Tickets Here
       </a>
     </main>
