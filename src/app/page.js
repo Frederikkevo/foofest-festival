0749e7ceb4202ee05e7be553789a50802c9277f3
Frederikke¨ <frvo0001@stud.kea.dk>
2024-12-19 21:19:19 +0100

    forsode

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
+import Header from '../components/Header';
+
 export default function Home() {
   return (
-    <main className="p-8 bg-cover bg-center" style={{ backgroundImage: "url('/images/background.jpg')" }}>
+    <main className="relative w-full min-h-screen p-8 bg-cover bg-center" style={{ backgroundImage: "url('/images/forside.webp')" }}>
       <Header />
-      <div className="max-w-7xl mx-auto">
-        <h2 className="text-primary text-3xl font-bold mt-6">Welcome to Foofest!</h2>
-        <p className="mt-4">Join us for an unforgettable experience of music, art, and community.</p>
-      </div>
     </main>
   );
 }
-
-
